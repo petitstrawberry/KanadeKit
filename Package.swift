591a589b4a56452e8ed93ac1ba39427371d00fb8
petitstrawberry <petitstrawberry.dev@gmail.com>
2026-04-19 18:57:39 +0900

    Update Package.swift and Package.resolved to include FLAC and Ogg dependencies

--- Package.swift
+++ Package.swift
@@ -15,11 +15,17 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.8"),
+        .package(url: "https://github.com/sbooth/flac-binary-xcframework", from: "0.2.0"),
+        .package(url: "https://github.com/sbooth/ogg-binary-xcframework", from: "0.1.3"),
     ],
     targets: [
         .target(
             name: "KanadeKit",
-            dependencies: [.product(name: "Starscream", package: "Starscream")],
+            dependencies: [
+                .product(name: "Starscream", package: "Starscream"),
+                .product(name: "FLAC", package: "flac-binary-xcframework"),
+                .product(name: "ogg", package: "ogg-binary-xcframework"),
+            ],
             path: "Sources/KanadeKit"
         ),
         .testTarget(
